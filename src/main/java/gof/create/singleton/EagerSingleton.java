cb9bb51c247b275985ce943f90cc21b9c37bb8f3
yangchaoy259189888 <17792035609@163.com>
2019-07-08 13:18:14 +0800

    饿汉式单例模式

--- src/main/java/gof/create/singleton/EagerSingleton.java
+++ src/main/java/gof/create/singleton/EagerSingleton.java
@@ -1,9 +1,5 @@
 package gof.create.singleton;
 
-/**
- * @author 西邮陈冠希
- * @date 2019/7/8
- **/
 public class EagerSingleton {
     private static final EagerSingleton instance = new EagerSingleton();
 
