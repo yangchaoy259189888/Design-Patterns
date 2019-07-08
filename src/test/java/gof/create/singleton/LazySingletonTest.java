527a01aea2a0b3f7a902714a7e4e45ced413c97d
yangchaoy259189888 <17792035609@163.com>
2019-07-08 13:03:39 +0800

    懒汉式junit测试

--- src/test/java/gof/create/singleton/LazySingletonTest.java
+++ src/test/java/gof/create/singleton/LazySingletonTest.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LazySingletonTest {
 
+    /**
+     *
+     * 懒汉式单例模式测试
+     */
     @Test
     void getInstance() {
         System.out.println(LazySingleton.getInstance());
